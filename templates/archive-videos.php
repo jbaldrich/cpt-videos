3f645f9ec2350b1b731bd83391d607a9415dc391
jbaldrich <jb@jacobaldrich.com>
2019-04-01 19:00:26 +0200

    remove "Archive" from title

--- templates/archive-videos.php
+++ templates/archive-videos.php
@@ -18,9 +18,9 @@ get_header(); ?>
 		?>
 
 			<header class="page-header">
+				<h1 class="page-title"><?php post_type_archive_title() ?></h1>
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					// the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
